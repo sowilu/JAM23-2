8185bfaad3e49d4b3efaf3f70f510f22071bf5fd
Viktor Kornilov <39262485+ViktorKornilov@users.noreply.github.com>
2023-03-25 19:02:16 +0200

    Spawner

--- Assets/Sim/Scripts/Spawner.cs
+++ Assets/Sim/Scripts/Spawner.cs
@@ -1,21 +1,59 @@
-using System.Collections;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using UnityEngine;
+using UnityEngine.Networking;
+using Random = UnityEngine.Random;
 
 public class Spawner : MonoBehaviour
 {
-    public float spawnRate = 0.1f;
-    public Transform target;
-    public float delay = 0.5f;
-    public Enemy enemyPrefab;
+    public float spawnRate = 0.5f;
+    public float spawnRateIncrease = 0.01f;
+
+    public List<Enemy> enemyPrefabs;
+
+    private List<Transform> spawnPoints;
+
+    private void Awake()
+    {
+        // find with tag
+        var gos = GameObject.FindGameObjectsWithTag("SpawnPoint");
+        spawnPoints = new List<Transform>();
+        foreach (var go in gos)
+        {
+            spawnPoints.Add(go.transform);
+        }
+    }
+
+
     void Start()
     {
-        InvokeRepeating(nameof(Spawn), delay, 1f / spawnRate);
+        SpawnRoutine();
+        
+    }
+
+    private void Update()
+    {
+        spawnRate += spawnRateIncrease * Time.deltaTime;
+    }
+
+
+    async void SpawnRoutine()
+    {
+        while (true)
+        {
+            Spawn();
+            await new WaitForSeconds(1 / spawnRate);
+        }
     }
 
     
     void Spawn()
     {
-        Instantiate(enemyPrefab, transform.position, transform.rotation).target = target;
+        var pos = spawnPoints[Random.Range(0, spawnPoints.Count)].position;
+        var prefab = enemyPrefabs[Random.Range(0, enemyPrefabs.Count)];
+
+        var obj = Instantiate(prefab, pos, transform.rotation);
+        obj.target = Player.inst.transform;
+
     }
 }
